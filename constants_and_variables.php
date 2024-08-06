b72079a39fec1b4f7dab7b7e81c415662de8633e
SantuMahata <156564496+SantuMahata@users.noreply.github.com>
2024-08-06 17:49:33 +0530

    Add files via upload

---
+++ constants_and_variables.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+$FirstName='gio';
+$FirstName='joe';
+echo $FirstName;
+*/
+
+
+// define('status','paid');
+// echo status;
+// echo "</br>";
+
+// define('Color','green');
+// echo Color;
+
+// echo "</br>";
+
+// const name = "santu";
+// echo name;
+
+// echo "</br>";
+
+// $paid ="paid";
+// define("status_".$paid,5);
+
+// echo $status_paid;
+
+// echo "</br>";
+
+
+// echo PHP_VERSION;
+
+
+
+
+// VARIABLES
+/*
+
+$foo="bar";
+$$foo="baz";
+echo "$foo,${$foo}";
+
+*/
+
+
+?>
