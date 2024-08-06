5149acacb3bd50c7509a035d4e2081f0f7027b8f
SantuMahata <156564496+SantuMahata@users.noreply.github.com>
2024-08-06 09:44:24 +0530

    Add files via upload

---
+++ basicsyntax.php
@@ -0,0 +1,108 @@
+<?php
+
+// echo "Hello World";
+// echo 'Hello World';
+// echo 'Hello',' ','World';
+// echo 'Joe\'s Invoice';
+# echo "Joe's Invoice";
+/*
+$name = 'Ajay';
+echo $name;
+*/
+/*
+$_name = 'Ajay';
+echo $_name;
+*/
+/*
+$x = 1;
+$y = $x;
+
+$x = 3;
+echo $y;
+*/
+/*
+$x = 1;
+$y = &$x;
+
+$x = 3;
+echo $y;
+*/
+/*
+$FirstName = 'Ajay';
+
+echo "Hello $FirstName";
+*/
+
+/*
+$FirstName = 'Ajay';
+
+echo "Hello {$FirstName}";
+*/
+
+/*
+$FirstName = 'Ajay';
+
+echo "Hello"." " . $FirstName;
+
+*/
+
+?>
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?php echo "Hello World"; ?>
+</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> -->
+
+
+<!-- 
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?= "Hello World"; ?>
+</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> -->
+
+
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?php
+    $x = 5;
+    $y = 6;
+
+    echo '<p>'.$x.','. $y.'</p>';
+    ?>
+</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> -->
+
+
+
+
+
+
+
+
+
+
+
