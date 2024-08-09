d43b800e018f12a505615b56c2ab5141f5ea66eb
SantuMahata <156564496+SantuMahata@users.noreply.github.com>
2024-08-09 21:04:11 +0530

    Add files via upload

---
+++ float_datatypes.php
@@ -0,0 +1,61 @@
+<?php
+
+$x = 5.99;
+
+echo $x;
+echo '</br>';
+var_dump($x);
+echo '</br>';
+
+
+$a=0.23;
+$b=1-0.77;
+
+if($a==$b){
+    echo "true";
+}else{
+    echo"false";
+}
+
+echo '</br>';
+
+
+$a = 0.23;
+$b = 1 - 0.77;
+
+if (abs($a - $b) < 0.00000001) {
+    echo "true";
+} else {
+    echo "false";
+}
+echo '</br>';
+
+echo PHP_FLOAT_MAX*2;
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$x = 10.365;
+var_dump($x);
+?>  
+
+</body>
+</html>
