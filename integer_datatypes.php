0cb038ac00fe64864deef2b60296474b21c33220
SantuMahata <156564496+SantuMahata@users.noreply.github.com>
2024-08-09 19:14:14 +0530

    Add files via upload

---
+++ integer_datatypes.php
@@ -0,0 +1,27 @@
+<?php
+
+$x = 5;
+$y = 55;
+$z = 055;
+echo $x;
+echo '</br>';
+echo $y;
+echo '</br>';
+echo $z;
+echo '</br>';
+
+$a= (int) 2.4;
+ echo $a;
+ echo '</br>';
+ var_dump($a);
+
+
+ $b = (int) '5.9';
+ echo $b;
+ echo '</br>';
+ var_dump($b);
+
+
+
+
+?>
