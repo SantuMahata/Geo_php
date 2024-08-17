98eef4962790eb8681607cc1cdd8ff77cd232e10
SantuMahata <156564496+SantuMahata@users.noreply.github.com>
2024-08-17 15:30:26 +0530

    Add files via upload

---
+++ 170824/null_datatypes.php
@@ -0,0 +1,47 @@
+<?php
+
+// null constant
+
+$x = null;
+echo $x;
+
+echo '<br/>';
+
+var_dump($x);
+
+echo '<br/>';
+
+var_dump(is_null($x));
+
+echo '<br/>';
+
+var_dump($x === null);
+
+echo '<br/>';
+var_dump((string)($x));
+
+ 
+echo '<br/>';
+var_dump((int)($x));
+
+
+echo '<br/>';
+
+var_dump((bool)($x));
+
+echo '<br/>';
+
+var_dump((array)($x));
+
+
+
+
+
+
+
+
+
+
+
+
+?>
